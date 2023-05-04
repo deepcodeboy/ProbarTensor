28bbf03117fcda90c2bb7d6562262124ae0dc1c7
deepcodeboy <ayrtonwe13@gmail.com>
2023-05-04 14:35:46 -0300

    agregado de ejercicio de pag 63

--- Tensor en Node/index.js
+++ Tensor en Node/index.js
@@ -16,6 +16,7 @@ const tensor2d = tf.tensor2d([[1, 2], [3, 4], [5, 6]]);
 tensor1d.print();
 tensor2d.print(); */
 
+//Ejercicio pagina 56 --------------------------------------------------------------------
 console.log('principio', tf.memory().numTensors)
 
 let keeper, chaser, seeker, beater;
@@ -38,6 +39,63 @@ keeper.dispose()
 chaser.dispose()
 
 console.log('fin', tf.memory().numTensors)
+//----------------------------------------------------------------------------------------
+
+//Ejercicio pagina 63 --------------------------------------------------------------------
+const users = ['Gant', 'Todd', 'Jed', 'Justin']
+const bands = [
+ 'Nirvana',
+ 'Nine Inch Nails',
+ 'Backstreet Boys',
+ 'N Sync',
+ 'Night Club',
+ 'Apashe',
+ 'STP'
+]
+const features = [
+ 'Grunge',
+ 'Rock',
+ 'Industrial',
+ 'Boy Band',
+ 'Dance',
+ 'Techno'
+]
+// User votes
+const user_votes = tf.tensor([
+ [10, 9, 1, 1, 8, 7, 8],
+ [6, 8, 2, 2, 0, 10, 0],
+ [0, 2, 10, 9, 3, 7, 0],
+ [7, 4, 2, 3, 6, 5, 5]
+])
+// Music Styles
+const band_feats = tf.tensor([
+ [1, 1, 0, 0, 0, 0],
+ [1, 0, 1, 0, 0, 0],
+ [0, 0, 0, 1, 1, 0],
+ [0, 0, 0, 1, 0, 0],
+ [0, 0, 1, 0, 0, 1],
+ [0, 0, 1, 0, 0, 1],
+ [1, 1, 0, 0, 0, 0]
+])
+
+// User's favorite styles
+const user_feats = tf.matMul(user_votes, band_feats)
+// Print the answers
+user_feats.print()
+
+// Let's make them pretty
+const top_user_features = tf.topk(user_feats, features.length)
+// Back to JavaScript
+const top_genres = top_user_features.indices.arraySync()
+// print the results
+users.map((u, i) => {
+ const rankedCategories = top_genres[i].map(v => features[v])
+ console.log(u, rankedCategories)
+})
+
+
+
+
 
 
 
